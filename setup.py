42403115d8d221620cd2f14d680cc68e3ac6ec5a
K. K <gerogero7429@gmail.com>
2022-03-05 12:42:09 +0900

    mk setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'kkgw',
+    version = '1.0.0',
+    url = 'https://github.com/keikagawa/kkgw.git',
+    license = 'Free',
+    author = 'keikagawa',
+    author_email = 'gerogero7429@gmail.com',
+    description = 'My function storage',
+    install_requires = ['setuptools'],
+    packages = ["kkgw", "kkgw.utils"],
+    # entry_points = {
+    #     'console_scripts': [
+    #         'top = kkgw.top:main',
+    #         'bottom = kkgw.utils.bottom:sub'
+    #     ]
+    }
+)
