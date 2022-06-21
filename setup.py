b4496a5eca871e4f00f5c654364657e72d93755a
K. K <gerogero7429@gmail.com>
2022-06-21 17:50:36 +0900

    update setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name = 'kkgw',
-    version = '1.0.0',
+    version = '1.1.0',
     url = 'https://github.com/keikagawa/kkgw.git',
     license = 'Free',
     author = 'keikagawa',
     author_email = 'gerogero7429@gmail.com',
     description = 'My function storage',
     install_requires = ['setuptools'],
-    packages = ["kkgw", "kkgw.utils"],
+    packages = ["kkgw", "kkgw.utils", "kkgw.math"],
     # entry_points = {
     #     'console_scripts': [
     #         'top = kkgw.top:main',
