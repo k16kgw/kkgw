8a74965f60c8b0e2eafc1348784c963d19886b1d
K. K <gerogero7429@gmail.com>
2022-06-24 11:27:05 +0900

    add DifferentialEquation HeatEq_ReactionBC_1d_DVDM

--- kkgw/math/DifferentialEquation.py
+++ kkgw/math/DifferentialEquation.py
@@ -1,6 +1,7 @@
 import numpy as np
+from slacker import Reactions
 
-class CahnHilliard_WithNeumannBC_ByDVDM():
+class CahnHilliardEq_NeumannBC_1d_DVDM():
 
     def __init__(
         self,
@@ -95,7 +96,7 @@ class CahnHilliard_WithNeumannBC_ByDVDM():
         output = (G.sum() - G[0]/2 - G[N-1]/2) * Dx
         return output
 
-class CahnHilliard_WithNeumannBC_ByFwdEuler():
+class CahnHilliardEq_NeumannBC_1d_FwdEuler():
 
     def __init__(
         self, 
@@ -187,42 +188,145 @@ class CahnHilliard_WithNeumannBC_ByFwdEuler():
         output = (G.sum() - G[0]/2 - G[N-1]/2) * Dx
         return output
 
-class HeatEq_ReactionBC_rect():
+class HeatEq_NeumannBC_1d_DVDM():
+    """
+    内部：熱方程式
+    境界：斉次ノイマン
+    領域：1次元
+    """
+
+    def __init__(self, cfg_inst):
+        """
+        CFG クラスの構成
+        ======
+        class CFG:
+            def __init__(
+                self,
+                date = '220623',
+                ver = 'fw-6',
+                settings: dict = {
+                    'N': 100, # 内部領域の分割数
+                    'Dx': 0.001, # 領域の分割幅
+                    'Dt': 0.0001, # 時間の分割幅
+                },
+                params: dict = {
+                    'Gamma': 2,
+                },
+                timeset: dict = {
+                    'inittime': 0, # 計算時の初期時刻
+                    'timespan': 5000, # 計算時の時間ステップ数
+                    'brank': 10, # 解を保存するステップ間隔
+                    'plt_inittime': 0, # グラフプロットでの初期時刻
+                    'plt_timespan': 5000, # グラフプロットでの時間ステップ数
+                },
+                output_dir = './data/output',
+            ):
+                self.settings = settings
+                self.params = params
+                self.timeset = timeset
+                self.output_dir = os.path.join(output_dir, date+'-'+ver)
+            
+            def initialdata(self, x):
+                return 関数(x)
+        """
+        self.cfg = cfg_inst
+        self.settings = self.cfg.settings # space dimension
+        self.params = self.cfg.params # parameters
+
+    def Laplacian(self, N) -> np.ndarray:
+        """ ラプラシアン """
+        output = np.zeros((N, N))
+        for k in range(1, N-1):
+            output[k, k-1:k+2] = [1, -2, 1]
+        return output
+
+    def equation(self, U2, U1) -> list:
+        """
+        DVDM
+        境界ぴったり
+        """
+        N = self.settings['N']
+        Dx = self.settings['Dx']
+        Dt = self.settings['Dt']
+        Gamma = self.params['Gamma']
+
+        Lap = self.Laplacian(N)
+
+        eq = [0]*(N)
+        eq[0] = U2[1]-U2[0] # eq[0] = - Gamma*(U1[1]-U1[0])/Dx
+        eq[1:N-1] = (U2[1:N-1]-U1[1:N-1])*Dx**2 - Gamma*0.5*np.dot(Lap, (U1+U2))[1:N-1]*Dt # (U2[1:N-1]-U1[1:N-1])/Dt - Gamma*np.dot(Lap, (U1+U2))[1:N-1]/(Dx**2)/2
+        eq[N-1] = U2[N-1]-U2[N-2] # eq[N-1] = + Gamma*(U1[N-1]-U1[N-2])/Dx
+
+        return eq
+
+
+class HeatEq_ReactionBC_1d_DVDM():
     """
     内部：熱方程式
     境界：反応方程式
-    領域：矩形
+    領域：1次元
     """
 
-    def __init__(
-        self, 
-        settings: dict = {
-            'N': 100, # 内部領域の分割数
-            'Dx': 0.01, # 領域の分割幅
-            'Dt': 0.01, # 時間の分割幅
-        },
-        params: dict = {
-            'Gamma': 1, # 拡散項の係数
-            'const': 1, # 反応項の係数
-        }
-    ):
-        self.settings = settings # space dimension
-        self.params = params # parameters
+    def __init__(self, cfg_inst):
+        """
+        CFG クラスの構成
+        ======
+        class CFG:
+            def __init__(
+                self,
+                date = '220623',
+                ver = 'fw-6',
+                settings: dict = {
+                    'N': 100, # 内部領域の分割数
+                    'Dx': 0.001, # 領域の分割幅
+                    'Dt': 0.0001, # 時間の分割幅
+                },
+                params: dict = {
+                    'Gamma': 2,
+                },
+                timeset: dict = {
+                    'inittime': 0, # 計算時の初期時刻
+                    'timespan': 5000, # 計算時の時間ステップ数
+                    'brank': 10, # 解を保存するステップ間隔
+                    'plt_inittime': 0, # グラフプロットでの初期時刻
+                    'plt_timespan': 5000, # グラフプロットでの時間ステップ数
+                },
+                output_dir = './data/output',
+            ):
+                self.settings = settings
+                self.params = params
+                self.timeset = timeset
+                self.output_dir = os.path.join(output_dir, date+'-'+ver)
+            
+            def initialdata(self, x):
+                return 関数(x)
+        """
+        self.cfg = cfg_inst
+        self.settings = self.cfg.settings # space dimension
+        self.params = self.cfg.params # parameters
 
     def Laplacian(self, N) -> np.ndarray:
-        """ 拡散項 """
-        output = np.zeros((N, N+2))
-        for k in range(0, N):
-            output[k, k:k+3] = [1, -2, 1]
+        """ ラプラシアン """
+        output = np.zeros((N, N))
+        for k in range(1, N-1):
+            output[k, k-1:k+2] = [1, -2, 1]
+        return output
+    
+    def Delx(self, Ubd, Uin):
+        """ 法線微分(向きも考慮されている) """
+        output = Uin - Ubd
         return output
 
-    def Reaction(self, U1):
+    def Reaction(self, U2, U1):
         """ 反応項 """
         N = self.settings['N']
         Dx = self.settings['Dx']
         const = self.params['const']
-
-        output = const * (U1[1:-1]**3 - U1[1:-1])
+        
+        output = const * (
+            (U2**3 + U2**2*U1 + U2*U1**2 + U1**3)
+            - (U2 + U1)
+        )
         return output
 
     def equation(self, U2, U1) -> list:
@@ -230,15 +334,14 @@ class HeatEq_ReactionBC_rect():
         N = self.settings['N']
         Dx = self.settings['Dx']
         Dt = self.settings['Dt']
+        Gamma = self.params['Gamma']
 
         Lap = self.Laplacian(N)
 
-        eq = [0]*(N+2)
+        eq = [0]*(N)
 
-        eq[0] = U2[0] - U2[4]
-        eq[1] = U2[1] - U2[3]
-        eq[2:N+2] = U2[2:N+2] - U1[2:N+2] + Dt/Dx**2 * np.dot(Lap, self.chem_func(U1))
-        eq[N+2] = U2[N+2] - U2[N]
-        eq[N+3] = U2[N+3] - U2[N-1]
+        eq[0] = (U2[0]-U1[0])*Dx - self.Reaction(U2[0], U1[0])*Dt*Dx + Gamma*0.5*(self.Delx(Ubd=U2[0]+U1[0], Uin=U2[1]+U1[1]))*Dt
+        eq[1:N-1] = (U2[1:N-1]-U1[1:N-1])*Dx**2 - Gamma*0.5*np.dot(Lap, (U1+U2))[1:N-1]*Dt # (U2[1:N-1]-U1[1:N-1])/Dt - Gamma*np.dot(Lap, (U1+U2))[1:N-1]/(Dx**2)/2
+        eq[N-1] = (U2[N-1]-U1[N-1])*Dx - self.Reaction(U2[N-1], U1[N-1])*Dt*Dx + Gamma*0.5*(self.Delx(Ubd=U2[N-1]+U1[N-1], Uin=U2[N-2]+U1[N-2]))*Dt
 
         return eq
--- kkgw/math/Simulation.py
+++ kkgw/math/Simulation.py
@@ -9,7 +9,7 @@ class Calc1d():
 
     def __init__(
         self,
-        CFG,
+        CFG, # class
     ):
         """
         CFG クラスの構成
@@ -22,6 +22,11 @@ class Calc1d():
                     'Dx': 0.5, # 領域の分割幅
                     'Dt': 0.5, # 時間の分割幅
                 },
+                timeset: dict = {
+                    'inittime': 0,
+                    'timespan': 1000,
+                    'brank': 10, # 解を保存するステップ間隔
+                },
                 output_dir = './data/output',
             ):
                 self.settings = settings
@@ -30,10 +35,11 @@ class Calc1d():
             def initialdata(self, x):
                 return x
         """
-        self.CFG = CFG()
-        self.settings = CFG().settings
-        self.output_dir = CFG().output_dir
-        self.initialdata = CFG().initialdata # function
+        self.cfg = CFG()
+        self.settings = self.cfg.settings
+        self.timeset = self.cfg.timeset
+        self.output_dir = self.cfg.output_dir
+        self.initialdata = self.cfg.initialdata # function
 
         OUTPUT_VAR = os.path.join(self.output_dir, 'var')
         os.makedirs(OUTPUT_VAR, exist_ok=True)
@@ -43,36 +49,28 @@ class Calc1d():
         """ 準備 """
         N = self.settings['N']
 
-        OUTPUT_U = os.path.join(self.output_dir, 'U')
+        OUTPUT_U = os.path.join(self.output_var, 'U')
         os.makedirs(OUTPUT_U, exist_ok=True)
 
         # 設定の保存
         with open(os.path.join(self.output_dir, 'CFG.pkl'), 'wb') as f:
-            pickle.dump(self.CFG, f)
+            pickle.dump(self.cfg, f)
         with open(os.path.join(self.output_dir, 'CFG.json'), 'w') as f:
-            json.dump(vars(self.CFG), f, indent=2)
+            json.dump(vars(self.cfg), f, indent=2)
 
         # 初期値の保存
         U = np.zeros((N, 2)) # 2ステップ分のU
-        for x in range(0, N):
-            U[x, 0] = self.initialdata(x)
+        for idx in range(0, N):
+            U[idx, 0] = self.initialdata(idx)
         np.save(os.path.join(OUTPUT_U, f't=0.0.npy'), U[:, 0])
 
-    def calc(
-        self,
-        equation,
-        timeset: dict = {
-            'inittime': 0,
-            'timespan': 1000,
-            'brank': 100, # 解を保存するステップ間隔
-            'Dt': 0.5 # settingsと同じ値
-        },
-    ):
+    def calc(self, equation):
+
         N = self.settings['N']
-        inittime = timeset['inittime']
-        timespan = timeset['timespan']
-        brank = timeset['brank']
-        Dt = timeset['Dt']
+        Dt = self.settings['Dt']
+        inittime = self.timeset['inittime']
+        timespan = self.timeset['timespan']
+        brank = self.timeset['brank']
 
         # 初期値の読み出し
         U = np.zeros((N, 2))
