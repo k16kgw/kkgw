83720b4d9a80e2dcba0fc5f3100ec6f83185f8a8
K. K <gerogero7429@gmail.com>
2022-06-24 11:32:40 +0900

    minorchg

--- kkgw/math/DifferentialEquation.py
+++ kkgw/math/DifferentialEquation.py
@@ -1,5 +1,4 @@
 import numpy as np
-from slacker import Reactions
 
 class CahnHilliardEq_NeumannBC_1d_DVDM():
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'kkgw',
-    version = '2.0.0',
+    version = '2.0.1',
     url = 'https://github.com/keikagawa/kkgw.git',
     license = 'Free',
     author = 'keikagawa',
